3c2f21e2ee8ba22a2ca25f6cb0863d54f8838558
jiy0ungkim <2626kjy@naver.com>
2025-09-25 16:06:33 +0900

    05

--- main.c
+++ main.c
@@ -3,16 +3,22 @@
 
 int main(int argc, char *argv[])
 {
-    int year;
-
-    printf("input the year : ");
-    scanf("%i", &year);
-
-    printf("is the year %i the leep year : %i\n", 
-                                           year,
-                                           ((year%4 == 0) && (year%100 != 0)) || (year%400 == 0));
+    unsigned int x;
+    int b;
     
+    printf("input a number : ");
+    scanf("%ui", &x);
+    
+    for (b=0; x != 0; x >>= 1)
+    {
+        if (x & 1)
+        {
+              b++;
+        }
+    }
+    
+    printf("The result is : %i\n", b);
+              
     system("PAUSE");	
     return 0;
 }
-
